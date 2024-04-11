09fef99610341a98b462848d9a8ff3b4fe220aa8
Charlton Teo <41657722+charl2on381@users.noreply.github.com>
2024-04-11 16:57:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/goit-react-hw-08-phonebookV2">
+  <BrowserRouter basename="/Phonebook-App">
     <Provider store={store}>
       <App />
       <ToastContainer />
